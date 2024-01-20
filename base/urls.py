b831f17062be47de168a88cbf9d8cf9729484d10
Gulo <gulonamikaze@gmail.com>
2024-01-20 23:12:46 +0400

    adding new pages

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('creator/', views.creator),
+    path('login/', views.login),
+    path('register/', views.register)
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserChangeForm
 
 # Create your views here.
 def home(request):
     return render(request, 'base/home.html')
+def creator(request):
+    return render(request, 'base/creator.html')
+
+def login(request):
+    return render(request, 'base/login.html')
+
+def register(request):
+    return render(request, 'base/register.html')
